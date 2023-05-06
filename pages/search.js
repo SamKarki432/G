81560f6b3cc738ce1b462ca35616cacad1e64c87
Samrakshan Karki <samrakshankarki1@gmail.com>
2023-05-06 09:39:14 +0545

    google api fetch and mock data

--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,11 @@
+import Response from "@/Response";
 import SearchHeader from "@/components/SearchHeader";
 import SearchHeaderOptions from "@/components/SearchHeaderOptions";
 import Head from "next/head";
 
 
-export default function Search(){
-
+export default function Search({results}){
+    console.log(results)
     return(
     <>
        <Head>
@@ -16,4 +17,18 @@ export default function Search(){
 
     </>
     )
+}
+
+export async function getServerSideProps(context){
+    const mockData = true;
+    const data = mockData? Response :await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`
+    ).then((response)=> response.json())
+
+    return{
+        props :{
+            results: data
+        }
+    }
+
 }
